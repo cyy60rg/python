c2eb9c4a1dd49e78ca900fedfdf562ce93e8ec7e
sreeshk692 <ksreesh28@gmail.com>
2014-10-16 08:54:51 +0530

    Create prog1.py

---
+++ prog1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+def main():
+ name="Sreesh K"
+ a=4
+ b=4.2
+ print "hello " + name +" "+ str(a+b)
+
+ name = raw_input('What is your name?\n')
+ print 'Hi, %s.' % name
+
+ c=input("Enter 1st number\n")
+ d=input("Enter 2nd number\n")
+ print "Sum" + str(c+d)
+
+main()
+~                                                                                   
+~                                                                                   
+~                     
